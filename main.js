3325187fa7025048c3d01df8fb96ddc277e5434a
James Meegan <jmeegan2@owlmail.harford.edu>
2021-06-18 02:31:17 -0400

    close to being done

---
+++ main.js
@@ -0,0 +1,14 @@
+const api = {
+    key: "afaf9f8d48cff6cafd32e23220bcfdbf",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+  
+  function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+  }
+  
